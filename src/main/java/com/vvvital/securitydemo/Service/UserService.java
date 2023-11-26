18019adba70fc7f2ebdcabe8a85186ebf949de97
Kovalenko Vitaliy <91799391+vvvital@users.noreply.github.com>
2023-11-26 12:12:42 +0200

    add swagger & security

--- src/main/java/com/vvvital/securitydemo/Service/UserService.java
+++ src/main/java/com/vvvital/securitydemo/Service/UserService.java
@@ -19,4 +19,12 @@ public class UserService {
     public List<User> findAll(){
         return userRepository.findAll();
     }
+
+    public void delete(User user){
+        userRepository.delete(user);
+    }
+
+    public User findById(Long id){
+        return userRepository.findById(id).orElse(null);
+    }
 }
---
+++ src/main/java/com/vvvital/securitydemo/SpringdocConfig.java
@@ -0,0 +1,15 @@
+//package com.vvvital.securitydemo;
+//
+//import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+//import io.swagger.v3.oas.models.OpenAPI;
+//import io.swagger.v3.oas.models.info.Info;
+//import org.springframework.context.annotation.Configuration;
+//
+//@OpenAPIDefinition
+//@Configuration
+//public class SpringdocConfig {
+//
+//    public OpenAPI openAPI () {
+//        return new OpenAPI().info(new Info().title("Marketplace").version("1.0").description("API doc"));
+//    }
+//}
